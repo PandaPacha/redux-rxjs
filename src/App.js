f001ce95ac6103cebe6369852b34248c90be8f6f
Marc FAGOT <marcfagot@PSD13465.local>
2018-04-04 10:30:04 +0200

    cleanup

--- src/App.js
+++ src/App.js
@@ -5,9 +5,7 @@ import { MouseClick } from './components/MouseClick'
 export class App extends Component {
   render() {
     return (
-      <div className="App">
         <MouseClick/>
-      </div>
     );
   }
 }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import { Providers } from './providers'
 import registerServiceWorker from './registerServiceWorker'
 
